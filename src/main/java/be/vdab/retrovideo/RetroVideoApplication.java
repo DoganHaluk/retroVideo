8fc6de1f389f37972f0ef3a9de91b7ce89081306
DoganHaluk <ha_look@yahoo.com>
2021-07-13 02:49:11 +0200

    Initial commit

---
+++ src/main/java/be/vdab/retrovideo/RetroVideoApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.retrovideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetroVideoApplication.class, args);
+    }
+
+}
